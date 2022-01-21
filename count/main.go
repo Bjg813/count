bbe8c13e6e34341a5f695fcc959b25d97988ba52
Brennan Grout <bjg813@gmail.com>
2022-01-20 18:24:49 -0800

    initial commit

---
+++ count/main.go
@@ -0,0 +1,16 @@
+// count tallies the number of times each line occurs within a file.
+package main
+
+import (
+	"count/datafile"
+	"fmt"
+	"log"
+)
+
+func main() {
+	lines, err := datafile.GetStrings("votes.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(lines)
+}
